248ccad6b8ca3ed26d6af8ee4500c9b442c909b3
Okabe <100302022+LabMember-001@users.noreply.github.com>
2022-06-21 00:31:20 +0200

    safari fullscreen size and exit scroll

--- swmp.meta.js
+++ swmp.meta.js
@@ -4,7 +4,7 @@
 // @namespace     LabMember-001
 // @author        https://github.com/LabMember-001
 // @license       GPLv3
-// @version       1.2.2
+// @version       1.2.3
 
 // @grant         none
 // @run-at        document-end
